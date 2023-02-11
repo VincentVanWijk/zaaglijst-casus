9f64da16bc04a5dc4a828e2cf47c0d70b805ab53
Vincent <vincent@avant.nl>
2023-02-11 21:52:41 +0100

    wip solving the gers casus

--- app/Http/Controllers/ProductionStateController.php
+++ app/Http/Controllers/ProductionStateController.php
@@ -2,7 +2,7 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use stdClass;
 
 class ProductionStateController extends Controller
 {
@@ -10,7 +10,60 @@ class ProductionStateController extends Controller
     {
         $productionStates = json_decode(file_get_contents(storage_path() . "/data/ProductieStaat.json"));
 
-        echo "<pre>";
-        print_r($productionStates);
+        $result = [];
+        foreach ($productionStates as $productionState) {
+            $saw = $productionState->saw;
+
+            foreach ($saw as $profileName => $profile) {
+                //check if this sawobject has a color object
+                if (!isset($saw->profielkleur->title)) {
+                    continue;
+                }
+                $color = $saw->profielkleur->title;
+
+                /** find all gnumber instances in the profile name
+                 *  pattern is either G or g, followed by one or more digits
+                 */
+                preg_match_all('/[gG]\d+/', $profileName, $matches);
+
+                // if no matches are found, continue to the next profile
+                if (!count($matches[0])) {
+                    continue;
+                }
+
+                foreach ($matches[0] as $gNumber) {
+                    $gNumber = strtoupper($gNumber);
+
+                    //create a new object with the count and length of the profile
+                    $data = new stdClass();
+                    $data->count = $profile->amount;
+                    $data->length = $profile->value;
+
+                    //check if the color and gNumber exist in the result array
+                    if (isset($result[$color][$gNumber])) {
+                        //check if this length already exists in the Gnumber array
+                        foreach ($result[$color][$gNumber] as $key => $existingProfile) {
+                            if ($existingProfile->length === $data->length) {
+                                //if the length already exists, add the count to the existing count
+                                $result[$color][$gNumber][$key]->count += $data->count;
+                                continue 2;
+                            }
+
+                            /**
+                             * if the length cannot be found in the array,
+                             * that means this is a new length, so add the new object
+                             */
+                            if ($key === array_key_last($result[$color][$gNumber])) {
+                                $result[$color][$gNumber][] = $data;
+                            }
+                        }
+                    } else {
+                        //add the new gNumber to the result array
+                        $result[$color][$gNumber] = [$data];
+                    }
+                }
+            }
+        }
+        dd($result);
     }
 }
